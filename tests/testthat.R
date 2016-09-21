7c44853ce49b15bc0d923928025cf6071fc9c312
Steven Mortimer <reportmort@gmail.com>
2016-09-21 17:30:03 -0400

    Revert Back to Old Way of Running Tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,9 @@
 library(testthat)
 library(rdfp)
 
-if (identical(tolower(Sys.getenv("NOT_CRAN")), "true") & identical(tolower(Sys.getenv("TRAVIS_PULL_REQUEST")), "false")) {
+if (identical(tolower(Sys.getenv("NOT_CRAN")), "true") & 
+    identical(tolower(Sys.getenv("TRAVIS_PULL_REQUEST")), "false")) {
   
-  devtools::test()
+  test_check('rdfp')
   
 }
