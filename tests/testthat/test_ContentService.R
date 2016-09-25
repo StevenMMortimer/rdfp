5fe8fb7baf9eb3b59d4b5a2fa13f233accb418df
Steven Mortimer <reportmort@gmail.com>
2016-09-25 00:07:25 -0400

    Turn off token cache to pass cran check

--- tests/testthat/test_ContentService.R
+++ tests/testthat/test_ContentService.R
@@ -2,6 +2,7 @@ context("ContentService")
 
 rdfp_options <- readRDS("rdfp_options.rds")
 options(rdfp.network_code = rdfp_options$network_code)
+options(rdfp.httr_oauth_cache = FALSE)
 options(rdfp.application_name = rdfp_options$application_name)
 options(rdfp.client_id = rdfp_options$client_id)
 options(rdfp.client_secret = rdfp_options$client_secret)
